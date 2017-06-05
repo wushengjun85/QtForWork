b2da15f2f34092c176db00e09e475c733c96d4f6
vmuser <vmuser@Linux-host.(none)>
2017-06-05 11:24:40 +0800

    update

--- XINYAN/ui_widget.h
+++ XINYAN/ui_widget.h
@@ -1,7 +1,8 @@
 /********************************************************************************
 ** Form generated from reading UI file 'widget.ui'
 **
-** Created by: Qt User Interface Compiler version 4.8.6
+** Created: Fri Jun 2 19:48:55 2017
+**      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
